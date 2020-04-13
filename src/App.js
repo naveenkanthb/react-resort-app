d6342572c09ca7fb5df95966b174644d81b09a3f
naveenkanthb <naveenkanth033@gmail.com>
2020-04-13 11:20:24 +0530

    commiting the boiler setup for the app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Error from './pages/Error';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+     React App
+    </div>
+  );
+}
+
+export default App;
